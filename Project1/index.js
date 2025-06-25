ecffcf8f654c2abca3357814725a8cf364985b3e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-25 19:34:40 +0530

    read, write and rename functions executed

--- Project1/index.js
+++ Project1/index.js
@@ -8,3 +8,34 @@ console.log("Next week date is: ", format(nextWeek, "yyyy-MM-dd"));
 
 const previousWeek = subDays(now,7);
 console.log("Previous week date was: ", format(previousWeek, "yyyy-MM-dd"));
+
+const fs = require("fs");
+
+//read a file
+fs.readFile("./example.txt","utf-8",(err,data)=>{
+  if(err){
+    console.log("Error reading the file: ",err);
+    return;
+  }
+  console.log("File contains: ", data);
+});
+
+//write to a file
+const content = "Hello Sibangi!!!";
+fs.writeFile("./example.txt",content,(err)=>{
+  if(err){
+    console.log("Error writing to the file: ",err);
+    return;
+  }
+  console.log("File written Successfully");
+});
+
+//rename a file
+fs.rename("./example.txt","new_example.txt",(err)=>{
+  if(err){
+    console.log("Error occured while renaming a file", err);
+    return;
+  }
+  console.log("File renamed Successfully");
+});
+
