4dddf962526959ced4b45c93fcdb6bd5bcce9308
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-26 09:56:58 +0530

    calculated the cpu length using os to know the size of thread pool

--- HelloWorld/file.js
+++ HelloWorld/file.js
@@ -1,3 +1,5 @@
 const fs = require("fs");
 //write file or create file
 fs.writeFileSync("./test.txt","Hey!! I am Sibangi");
+const os = require("os");
+console.log(os.cpus().length); //8 is the number of cpus so my thread pool can be of size 8 for blocking operations
