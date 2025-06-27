9987eb109a7d67d2be0341a84db66b8dd4abb434
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-27 14:59:10 +0530

    added various http request methods and accessing different parameters in url

--- ExpressProject/src/index.js
+++ ExpressProject/src/index.js
@@ -3,8 +3,32 @@ const app = express();
 const port = 3001;
 
 //get route
-app.get("/",(req,res)=>{
-  console.log("Test");
+app.get("/:category",(req,res)=>{
+  console.log("Request URL: ", req.url);
+  console.log("Request params: ",req.params);
+  console.log("Request query: ",req.query);
+  console.log("Request headers: ",req.headers);
+  console.log("Request Method: ",req.method);
+  res.send("Hello World");
+});
+
+app.post("/",(req,res)=>{
+  console.log("Request Method: ",req.method);
+  res.send("Hello World");
+});
+
+app.patch("/",(req,res)=>{
+  console.log("Request Method: ",req.method);
+  res.send("Hello World");
+});
+
+app.put("/",(req,res)=>{
+  console.log("Request Method: ",req.method);
+  res.send("Hello World");
+});
+
+app.delete("/",(req,res)=>{
+  console.log("Request Method: ",req.method);
   res.send("Hello World");
 });
 
