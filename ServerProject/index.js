745e509941c9180f27d36ff83f7eadd2bd1fd71f
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-29 12:36:40 +0530

    added http methods

--- ServerProject/index.js
+++ ServerProject/index.js
@@ -5,13 +5,13 @@ const url = require("url");
 //we need a handler to handle the requests
 const myServer = http.createServer((req,res)=>{
   if(req.url=='/favicon.ico') return res.end();
-  const log = `${Date.now()}: ${req.url} New Request received \n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New Request received \n`;
   const myurl = url.parse(req.url,true);
   console.log(myurl);
   fs.appendFile("./log.txt",log,(err, data)=>{
       switch(myurl.pathname){
         case "/":
-          res.end("Home Page");
+          if(req.method=="GET") res.end("Home Page");
           break;
         case "/about":
           const username = myurl.query.myname;
@@ -20,6 +20,14 @@ const myServer = http.createServer((req,res)=>{
         case "/contacts":
           res.end("010028564");
           break;
+        case "/signup":
+          if(req.method=="GET"){
+            res.end("This is a signup form.");
+          }
+          else if (req.method=="POST"){
+            //DB query
+            res.end("Success");
+          }
         default:
           res.end("404 Not found");
       }
