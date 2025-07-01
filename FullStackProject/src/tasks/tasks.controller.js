c2865d01a12d69fcf3df0abde0006eea064b8a37
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-01 21:47:07 +0530

    used the status code package

--- FullStackProject/src/tasks/tasks.controller.js
+++ FullStackProject/src/tasks/tasks.controller.js
@@ -1,5 +1,23 @@
+const {StatusCodes, ReasonPhrases} = require("http-status-codes");
+
 function handleGetTasks(req,res){
-  res.send("GET tasks controller");
+  let response = [
+    {
+      "title": "Title of the task1",
+      "date": "2025-01-01T12:00:00Z",
+      "description": "Task1 is to be done",
+      "priority": "normal",
+      "status": "todo"
+    },
+    {
+      "title": "Title of the task2",
+      "date": "2025-01-01T12:00:00Z",
+      "description": "Task2 is to be done.",
+      "priority": "normal",
+      "status": "todo"
+    },
+  ];
+  res.status(StatusCodes.OK).json(response);
 }
 function handlePostTasks(req,res){
   console.log("Request Body:", req.body);
