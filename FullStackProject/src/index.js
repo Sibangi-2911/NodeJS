6acb32648b9197a14b144ce6763ace4b8d77d42d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-01 21:48:01 +0530

    made the necessary changes in the code

--- FullStackProject/src/index.js
+++ FullStackProject/src/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
+const responseFormatter = require("./middleware/responseFormatter.js");
+const { StatusCodes } = require("http-status-codes");
 const taskRouter = require("./tasks/tasks.router.js");
+const authRouter = require("./auth/auth.router.js");
+const usersRouter = require("./users/users.router.js");
 const cors = require("cors");
 
 const app = express();
@@ -26,9 +30,17 @@ let accessLogStream = fs.createWriteStream(path.join(__dirname,"..","access.log"
   flag:'a',
 });
 app.use(morgan("combined", {stream: accessLogStream}));
+app.use(responseFormatter);
 
 //attach the router here that is define the routes
 app.use("/", taskRouter);
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+
+//if resource/request not found
+app.use((req,res)=>{
+  res.status(StatusCodes.NOT_FOUND).json(null);
+})
 
 app.listen(port,()=>{
   console.log(`App listens at port no. ${port}`);
