9d96195eff2d1b7a21fd3e11086e95cb854abfb0
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-01 16:29:00 +0530

    added middlewares, cors and attached router for the task project

--- FullStackProject/src/index.js
+++ FullStackProject/src/index.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const taskRouter = require("./tasks/tasks.router.js");
+const cors = require("cors");
+
 const app = express();
 const port = 3001;
 
-//get route
-app.get("/users/:role",(req,res)=>{
-  console.log("Request Parmeters: ",req.params);
-  res.send("Hello World");
-});
+/*const middleware = function(req,res,next){
+  req.info = {appname: "Tasks Manager", author: "Sibangi"};
+  next();
+};
+app.use(middleware);*/
+app.use(express.json()); // this middleware converts the incoming request body into json
 
+//on;y to these origins the resources could be shared
+const corsOptions = {
+  origin: ["example.com","example2.com"],
+};
+app.use(cors()); //include all origins only during development otherwise specify clear origins from where the request can be sent
+
+//flag a means append the next user activty
+let accessLogStream = fs.createWriteStream(path.join(__dirname,"..","access.log"),{
+  flag:'a',
+});
+app.use(morgan("combined", {stream: accessLogStream}));
+
+//attach the router here that is define the routes
+app.use("/", taskRouter);
 
 app.listen(port,()=>{
   console.log(`App listens at port no. ${port}`);
