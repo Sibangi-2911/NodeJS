dc3311e1d5e79a39afd2ddb39cdf1295d05f0704
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-02 15:29:11 +0530

    connected the mongodb using mongoose by creating schema structure and model

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,11 +1,41 @@
 //Restapi json project
 const express = require("express");
 const fs = require("fs");
+const mongoose = require("mongoose");
 const users = require("./MOCK_DATA.json");
 const { stringify } = require("querystring");
 const { error } = require("console");
+const { type } = require("os");
 const app = express();
 const PORT = 8000;
+
+//schema- Defining the structure
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type:String,
+    required:true,
+  },
+  lastName: {
+    type:String,
+  },
+  email: {
+    type:String,
+    required:true,
+    unique:true,
+  },
+  jobTitle: {
+    type:String,
+  },
+  gender: {
+    type: String,
+  },
+});
+//connection
+mongoose.connect("mongodb://127.0.0.1:27017/new-app-1")
+.then(()=> console.log("MongoDB Connected"))
+.catch((err)=> console.log("Mongo Error", err));
+//schema model
+const User = mongoose.model("user", userSchema);
 
 //middleware plugin
 app.use(express.urlencoded({extended: false}));
