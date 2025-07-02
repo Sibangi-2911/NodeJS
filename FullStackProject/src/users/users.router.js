809c7bfb77a0849f9914717116ff91468608ee91
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-02 23:15:40 +0530

    created a directory that contains all files of users

---
+++ FullStackProject/src/users/users.router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const usersController = require("./users.controller.js");
+const usersRouter = express.Router();
+
+usersRouter.post("/create", usersController.handleCreateUser);
+
+module.exports = usersRouter;
