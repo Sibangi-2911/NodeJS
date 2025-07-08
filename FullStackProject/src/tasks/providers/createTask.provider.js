5bafe5155ff1077065def1f74e09b3bcb3f62a1c
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-09 03:24:53 +0530

    added the matched data method of express validator for sanitized data

--- FullStackProject/src/tasks/providers/createTask.provider.js
+++ FullStackProject/src/tasks/providers/createTask.provider.js
@@ -1,14 +1,20 @@
 const Task = require("../task.schema.js");
+const {matchedData} = require("express-validator");
+const {StatusCodes} = require("http-status-codes");
 
 async function createTaskProvider(req,res) {
-  const task = new Task({
-    title: req.body.title,
-    description: req.body.description,
-    status: req.body.status,
-    priority:req.body.priority,
-    dueDate: req.body.dueDate,
-  });
-  return await task.save();
+  const validatedResult = matchedData(req);
+  console.log(validatedResult);
+  const task = new Task(validatedResult);
+  try{
+    await task.save();
+    return res.status(StatusCodes.CREATED).json(task);
+  }catch(error){
+    console.log(error);
+    return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
+  }
 }
 
 module.exports = createTaskProvider;
