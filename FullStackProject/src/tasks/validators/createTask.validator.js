9cef267f15f539db7184d4d205198cb0528737a0
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-08 20:55:03 +0530

    this file contains the validator for create task

---
+++ FullStackProject/src/tasks/validators/createTask.validator.js
@@ -0,0 +1,15 @@
+const {body} = require("express-validator");
+
+const createTaskValidator = [
+  body("title", "The title should not be empty").notEmpty(),
+  body("title","The title should be a string").isString(),
+  body("title").trim(),
+  body("title").isLength({max:100}),
+  body("dueDate","dueDate needs to be a valid ISO8601 string").isString().isISO8601().notEmpty(),
+  body("description", "The description cannot be empty and needs to be a string").isString().notEmpty().trim(),
+  body("description", "The description cannot be more than 500 characters.").isLength({max:500}),
+  body("priority").isIn(["low","normal","high"]),
+  body("status").isIn(["todo","inProgress","completed"]),
+];
+
+module.exports = createTaskValidator;
