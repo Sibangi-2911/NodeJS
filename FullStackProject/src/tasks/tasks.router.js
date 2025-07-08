735d0755eecafd969af92186a42190965393b3e4
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-08 20:56:51 +0530

    added the express validator to post router

--- FullStackProject/src/tasks/tasks.router.js
+++ FullStackProject/src/tasks/tasks.router.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const {body, validationResult} = require("express-validator");
 const taskController = require("./tasks.controller");
+const { StatusCodes } = require("http-status-codes");
+const createTaskValidator = require("./validators/createTask.validator.js");
 const taskRouter = express.Router();
 
 taskRouter.get("/tasks",taskController.handleGetTasks);
 
-taskRouter.post("/tasks", taskController.handlePostTasks);
+taskRouter.post("/tasks", createTaskValidator ,(req,res)=>{ 
+  const result = validationResult(req);
+  if(result.isEmpty()){
+    return taskController.handlePostTasks(req,res);
+  } else{
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
+  }
+}
+);
 
 taskRouter.patch("/tasks", taskController.handlePatchTasks);
 
