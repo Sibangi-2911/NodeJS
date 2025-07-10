ab14ea32c3fd5de354e75f888e4f3932a285b7b9
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-10 23:11:27 +0530

    added the logger, errorLogger and validator

--- FullStackProject/src/tasks/providers/updateTask.provider.js
+++ FullStackProject/src/tasks/providers/updateTask.provider.js
@@ -1,18 +1,32 @@
 const Task = require("../task.schema.js");
+const {matchedData} = require("express-validator");
+const {StatusCodes} = require("http-status-codes");
+const errorLogger = require("../../helpers/errorLogger.helper.js");
 
 async function updateTaskProvider(req,res){
-  //fetch id
-  const task = await Task.findById(req.body["_id"]);
+  const validatedData = matchedData(req);
+  try{
+    //fetch id
+  const task = await Task.findById(req.body["_id"]).exec();
 
   //update the task
-  task.title= req.body.title;
-  task.description=req.body.description;
-  task.status=req.body.status;
-  task.priority = req.body.priority;
-  task.dueDate = req.body.dueDate;
+  task.title= validatedData.title || task.title;
+  task.description=validatedData.description || task.description;
+  task.status=validatedData.status || task.status;
+  task.priority = validatedData.priority || task.priority;
+  task.dueDate = validatedData.dueDate || task.dueDate;
 
   //save
-  return await task.save();
+  await task.save();
+
+  return res.status(StatusCodes.OK).json(task);
+  }catch(error){
+    errorLogger("Error while fetching the tasks", req, error);
+    return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
+  }
+  
 }
 
 module.exports = updateTaskProvider;
