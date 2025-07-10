56d9f86fb66d1d7756caa437351eb54de95c90e1
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-10 22:30:55 +0530

    added the logger, errorLogger and validator

--- FullStackProject/src/tasks/providers/createTask.provider.js
+++ FullStackProject/src/tasks/providers/createTask.provider.js
@@ -1,6 +1,8 @@
 const Task = require("../task.schema.js");
 const {matchedData} = require("express-validator");
 const {StatusCodes} = require("http-status-codes");
+const logger = require("../../helpers/winston.helper.js");
+const errorLogger = require("../../helpers/errorLogger.helper.js");
 
 async function createTaskProvider(req,res) {
   const validatedResult = matchedData(req);
@@ -11,6 +13,8 @@ async function createTaskProvider(req,res) {
     return res.status(StatusCodes.CREATED).json(task);
   }catch(error){
     console.log(error);
+    errorLogger(`Error creating a new task: ${error.message}`, req, error);
+    
     return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
       reason: "Unable to process your request at the moment, please try later.",
     });
