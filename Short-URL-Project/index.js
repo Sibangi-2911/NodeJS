58da0dd123b0ded3c70469cf92faa37c57e7ca6b
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-12 16:55:13 +0530

    ejs view engine , path and static router added

--- Short-URL-Project/index.js
+++ Short-URL-Project/index.js
@@ -1,18 +1,36 @@
 const express = require("express");
+const path = require("path");
 const {connectToMongodb} = require("./connect.js");
 const urlRoute = require("./routes/url.js");
+const staticRouter = require("./routes/staticRouter.js");
 const URL = require("./models/url.js");
+
 
 const app = express();
 const PORT = 8001;
 
 connectToMongodb('mongodb://localhost:27017/short-url');
 
+//server side rendering ejs
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-app.use("/URL", urlRoute);
+//html page
+/*app.get("/test", async(req,res)=>{
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls:allUrls,
+  });
+});*/
 
-app.get("/:shortId", async (req,res)=>{
+app.use("/url", urlRoute);
+
+app.use("/",staticRouter);
+
+app.get("/url/:shortId", async (req,res)=>{
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate({
     shortId,
