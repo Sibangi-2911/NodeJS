aa7c089b4aa0fad75ef3584b6de5bb9db57defc6
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-13 23:49:01 +0530

    Basic Authentication setup is done

--- Short-URL-Project/index.js
+++ Short-URL-Project/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const {connectToMongodb} = require("./connect.js");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth.js");
 const urlRoute = require("./routes/url.js");
 const staticRouter = require("./routes/staticRouter.js");
+const userRoute = require("./routes/user.js");
+
 const URL = require("./models/url.js");
 
 
@@ -17,6 +21,7 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
 
 //html page
 /*app.get("/test", async(req,res)=>{
@@ -26,9 +31,11 @@ app.use(express.urlencoded({extended:false}));
   });
 });*/
 
-app.use("/url", urlRoute);
+app.use("/url",restrictToLoggedinUserOnly, urlRoute);
 
-app.use("/",staticRouter);
+app.use("/",checkAuth,staticRouter);
+
+app.use("/user",userRoute);
 
 app.get("/url/:shortId", async (req,res)=>{
   const shortId = req.params.shortId;
