f1d807fec2513765170f38c79dfd57a1774074ed
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-14 23:08:10 +0530

    added some if conditions to get the desired response

--- FullStackProject/src/middleware/responseFormatter.js
+++ FullStackProject/src/middleware/responseFormatter.js
@@ -1,4 +1,5 @@
 const {getReasonPhrase} = require("http-status-codes");
+const { error } = require("winston");
 
 function responseFormatter(req,res,next){
   const originalJson = res.json;
@@ -8,9 +9,18 @@ function responseFormatter(req,res,next){
       status: res.statusCode >=200 && res.statusCode<300 ? "success":"error",
       statusCode: res.statusCode,
       message : getReasonPhrase(res.statusCode),
-      data:res.statusCode >=200 && res.statusCode<300 ? data : null,
-      error:res.statusCode >=200 && res.statusCode<300 ? null: data,
+      /*data:res.statusCode >=200 && res.statusCode<300 ? data : null,
+      error:res.statusCode >=200 && res.statusCode<300 ? null: data,*/
     };
+    if(res.statusCode >=200 && res.statusCode<300){
+      response.data = data.pagination ? data.data : data;
+    }
+    if(res.statusCode>300){
+      response.error = error;
+    }
+    if(data.pagination){
+      response.pagination = data.pagination;
+    }
     originalJson.call(res,response); // res the original object is now replaced by response
   };
   next();
