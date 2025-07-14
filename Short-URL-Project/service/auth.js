3a02702429853437d6982b7942a57814ae4a2607
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-14 19:36:55 +0530

    used the jwt(stateless authentication) instead of sessionid(stateful authentication)

--- Short-URL-Project/service/auth.js
+++ Short-URL-Project/service/auth.js
@@ -1,11 +1,21 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Sibangi$123@$";
 
-function setUser(id, user){
-  sessionIdToUserMap.set(id, user);
+//this function will make the tokens
+function setUser(user){
+  return jwt.sign({
+    _id: user._id,
+    email:user.email,
+  }, secret);
 }
 
-function getUser(id){
-  return sessionIdToUserMap.get(id);
+function getUser(token){
+  if(!token) return null;
+  try{
+    return jwt.verify(token, secret);
+  }catch(error){
+    return null;
+  }
 }
 
 
