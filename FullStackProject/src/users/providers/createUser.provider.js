34e75b5179fcb53e744c874ea4a865125ca7999a
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-15 16:48:28 +0530

    hashing, salting and uniqueness of email implemented

--- FullStackProject/src/users/providers/createUser.provider.js
+++ FullStackProject/src/users/providers/createUser.provider.js
@@ -1,25 +1,47 @@
 const User = require("../user.schema.js");
-const {matchedData} = require("express-validator");
-const {StatusCodes} = require("http-status-codes");
+const { matchedData } = require("express-validator");
+const { StatusCodes } = require("http-status-codes");
 const errorLogger = require("../../helpers/errorLogger.helper.js");
+const bcrypt = require("bcrypt");
+const getUserByEmail = require("./getUserByEmail.provider.js");
 
-async function createUserProvider(req,res) {
+async function createUserProvider(req, res) {
   const validatedData = matchedData(req);
-  try{
+
+  try {
+    const existingUser = await User.findOne({ email: validatedData.email.trim().toLowerCase() });
+
+    if (existingUser) {
+      console.log("User already exists:", existingUser.email);
+      return res.status(StatusCodes.CONFLICT).json({
+        reason: "A user with this email already exists",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(validatedData.password, salt);
+
     const user = new User({
-    firstName:validatedData.firstName,
-    lastName:validatedData.lastName,
-    email:validatedData.email,
-    password:validatedData.password,
-  });
-  await user.save();
-  delete user.password;
-  return res.status(StatusCodes.CREATED).json(user);
-  }catch(error){
+      firstName: validatedData.firstName.trim(),
+      lastName: validatedData.lastName.trim(),
+      email: validatedData.email.trim().toLowerCase(),
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    return res.status(StatusCodes.CREATED).json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    });
+
+  } catch (error) {
     errorLogger("Error while creating a user", req, error);
-        return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
-          reason: "Unable to process your request at the moment, please try later.",
-        });
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
   }
 }
 
