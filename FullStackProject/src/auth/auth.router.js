6460fe821ddd6f5d5ec326b702e4900b2a5855b7
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-15 16:53:41 +0530

    validator and controller added

--- FullStackProject/src/auth/auth.router.js
+++ FullStackProject/src/auth/auth.router.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const authController = require("./auth.controller");
+const {StatusCodes} = require("http-status-codes");
+const loginValidator = require("./validators/login.validator.js");
+const {validationResult} = require("express-validator");
 const authRouter = express.Router();
 
 
-authRouter.post("/login",authController.handleLogin);
+authRouter.post("/login",loginValidator,(req,res)=>{
+  const result = validationResult(req);
+    if(result.isEmpty()){
+      return authController.handleLogin(req,res);
+    } else{
+      res.status(StatusCodes.BAD_REQUEST).json(result.array());
+    }
+});
 
 module.exports = authRouter;
