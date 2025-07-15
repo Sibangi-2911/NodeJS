9a5d1fa2110628f9317c6a9a0301aca73c7bd3a6
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-15 16:39:52 +0530

    created the login provider to get the hash and compare with the entered password

---
+++ FullStackProject/src/auth/providers/login.provider.js
@@ -0,0 +1,29 @@
+const {matchedData} = require("express-validator");
+const {StatusCodes} = require("http-status-codes");
+const errorLogger = require("../../helpers/errorLogger.helper.js");
+const getUserByEmail = require("../../users/providers/getUserByEmail.provider.js");
+const bcrypt = require("bcrypt");
+
+async function loginProvider(req,res) {
+  const validatedData = matchedData(req);
+  try{
+    //get user from database
+    const user = await getUserByEmail(validatedData.email);
+
+    //compare hash
+    const result = await bcrypt.compare(validatedData.password, user.password);
+
+    if(!result){
+      return res.status(StatusCodes.BAD_REQUEST).json({message: "Please check your credentials"});
+    }
+
+    return res.status(StatusCodes.OK).json({login:true});
+  }catch(error){
+    errorLogger("Error while trying to login", req, error);
+    return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
+  }
+}
+
+module.exports = loginProvider;
