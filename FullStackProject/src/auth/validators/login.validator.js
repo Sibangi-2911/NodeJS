93a7bde7c78286bccd7c4fd65ffb4431939229e5
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-15 16:40:56 +0530

    login validator created

---
+++ FullStackProject/src/auth/validators/login.validator.js
@@ -0,0 +1,10 @@
+const {body} = require("express-validator");
+
+
+const loginValidator = [
+  body("email").isEmail().notEmpty().trim(),
+  body("password").isLength({min:8}).isString(),
+];
+
+
+module.exports = loginValidator;
