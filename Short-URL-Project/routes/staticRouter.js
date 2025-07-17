189a4238df88ce0d74a66ebfe7865c2235693125
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-17 16:31:08 +0530

    added a new router with authorization of roles of user

--- Short-URL-Project/routes/staticRouter.js
+++ Short-URL-Project/routes/staticRouter.js
@@ -1,10 +1,17 @@
 //static router meams used for front end
 const express = require("express");
+const {restrictTo} = require("../middlewares/auth.js");
 const URL = require("../models/url.js");
 const router = express.Router();
 
-router.get("/",async(req,res)=>{
-  if(!req.user) return res.redirect("/login")
+router.get("/admin/urls", restrictTo(["ADMIN"]), async(req,res)=>{
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls:allUrls,
+  });
+})
+
+router.get("/",restrictTo(["NORMAL", "ADMIN"]),async(req,res)=>{
   const allUrls = await URL.find({ createdBy: req.user._id});
   return res.render("home", {
     urls:allUrls,
