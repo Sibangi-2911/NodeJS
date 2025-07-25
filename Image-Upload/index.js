e8d3a97080e65d7ed999b20d6b4a88121003b9d5
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-25 14:24:15 +0530

    added the diskstorage and completed the code for file uploads

--- Image-Upload/index.js
+++ Image-Upload/index.js
@@ -1,16 +1,36 @@
 const path = require("path");
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const PORT = 8000;
 
+//upload is a middleware whateverfile will be uploaded put that file in uploads folder
+//const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    return cb(null,"./uploads");
+  },
+  filename: function(req,file,cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({storage});
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.json());
+app.use(express.urlencoded({extended: false})); //to parse form data we use this middleware
 
-app.use("/", (req,res)=>{
+app.get("/", (req,res)=>{
   return res.render("homepage");
+});
+
+app.post("/upload", upload.single("profileImage") ,(req,res)=>{
+  console.log(req.body);
+  console.log(req.file);
+  return res.redirect("/");
 });
 
 app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`));
